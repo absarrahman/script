74fec2fe1e0688378914124642fbf7b0f2680c92
Irvin <33453856+irvin016@users.noreply.github.com>
2017-12-31 13:21:56 +0000

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/irvin016/device_xiaomi_kenzo -b oreo-mr1 device/xiaomi/kenzo
 git clone https://github.com/irvin016/device_xiaomi_msm8956-common -b oreo-mr1 device/xiaomi/msm8956-common
-git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b oreo-mr1 kernel/xiaomi/msm8956
+git clone https://github.com/energyspear17/Shadow-Kenzo -b O-buildable kernel/xiaomi/msm8956
 git clone https://github.com/irvin016/proprietary_vendor_xiaomi-1 -b oreo vendor/xiaomi
 git clone https://github.com/AmolAmrit/android_hardware_cyanogen -b oreo hardware/cyanogen
 rm -rf hardware/qcom/audio-caf/msm8952
