38d2bfdfadcc4c1fac4383f7e83e1bf5b327aac3
Irvin <33453856+irvin016@users.noreply.github.com>
2018-01-06 12:10:30 +0530

    Major Update  -:_:-

--- pixel.sh
+++ pixel.sh
@@ -1,16 +1,15 @@
-git clone https://github.com/irvin016/device_xiaomi_kenzo -b oreo-mr1 device/xiaomi/kenzo
-git clone https://github.com/irvin016/device_xiaomi_msm8956-common -b oreo-mr1 device/xiaomi/msm8956-common
-git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b oreo-mr1 kernel/xiaomi/msm8956
-git clone https://github.com/irvin016/proprietary_vendor_xiaomi-1 -b oreo vendor/xiaomi
-git clone https://github.com/AmolAmrit/android_hardware_cyanogen -b oreo hardware/cyanogen
+git clone https://github.com/irvin016/android_device_xiaomi_kenzo -b o-mr1 device/xiaomi/kenzo
+git clone https://github.com/irvin016/android_kernel_xiaomi_kenzo -b o-mr1 kernel/xiaomi/kenzo
+git clone https://github.com/irvin016/android_vendor_xiaomi_kenzo -b o-mr1 vendor/xiaomi/kenzo
 rm -rf hardware/qcom/audio-caf/msm8952
 rm -rf hardware/qcom/display-caf/msm8952
 rm -rf hardware/qcom/media-caf/msm8952
-git clone https://github.com/Cardinal-AOSP/hardware_qcom_audio -b oreo-mr1-caf-8952 hardware/qcom/audio-caf/msm8952
-git clone https://github.com/Cardinal-AOSP/hardware_qcom_media -b oreo-mr1-caf-8952 hardware/qcom/media-caf/msm8952
-git clone https://github.com/Cardinal-AOSP/hardware_qcom_display -b oreo-mr1-caf-8952 hardware/qcom/display-caf/msm8952
+git clone https://github.com/irvin016/android_hardware_qcom_audio -b o-mr1 hardware/qcom/audio-caf/msm8952
+git clone https://github.com/irvin016/android_hardware_qcom_media -b o-mr1 hardware/qcom/media-caf/msm8952
+git clone https://github.com/irvin016/android_hardware_qcom_display -b o-mr1 hardware/qcom/display-caf/msm8952
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
-
+git clone https://github.com/irvin016/android_external_ant-wireless_antradio-library -b staging/lineage-15.1
+ external_ant-wireless_antradio-library
 
 
