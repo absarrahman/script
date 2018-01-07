3f7ec9e9b44236ebe292104fbd9435b398a0d7db
Irvin <33453856+irvin016@users.noreply.github.com>
2018-01-07 09:46:52 +0000

    fix derp

--- pixel.sh
+++ pixel.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/irvin016/device_xiaomi_kenzo -b o-mr1 device/xiaomi/kenzo
-git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b o-mr1 device/xiaomi/msm8956-common
+git clone https://github.com/irvin016/device_xiaomi_msm8956-common -b o-mr1 device/xiaomi/msm8956-common
 git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b o-mr1 kernel/xiaomi/msm8956
 git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b o-mr1 vendor/xiaomi/kenzo
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
