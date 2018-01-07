fb601f0e8a4222f73b3156a07506ddece2f63927
Irvin <33453856+irvin016@users.noreply.github.com>
2018-01-07 09:42:32 +0000

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -1,15 +1,9 @@
-git clone https://github.com/irvin016/android_device_xiaomi_kenzo -b o-mr1 device/xiaomi/kenzo
-git clone https://github.com/irvin016/android_kernel_xiaomi_kenzo -b o-mr1 kernel/xiaomi/kenzo
-git clone https://github.com/irvin016/android_vendor_xiaomi_kenzo -b o-mr1 vendor/xiaomi/kenzo
-rm -rf hardware/qcom/audio-caf/msm8952
-rm -rf hardware/qcom/display-caf/msm8952
-rm -rf hardware/qcom/media-caf/msm8952
-git clone https://github.com/irvin016/android_hardware_qcom_audio -b o-mr1 hardware/qcom/audio-caf/msm8952
-git clone https://github.com/irvin016/android_hardware_qcom_media -b o-mr1 hardware/qcom/media-caf/msm8952
-git clone https://github.com/irvin016/android_hardware_qcom_display -b o-mr1 hardware/qcom/display-caf/msm8952
+git clone https://github.com/irvin016/device_xiaomi_kenzo -b o-mr1 device/xiaomi/kenzo
+git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b o-mr1 device/xiaomi/msm8956-common
+git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b o-mr1 kernel/xiaomi/msm8956
+git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b o-mr1 vendor/xiaomi/kenzo
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
-
 git clone https://github.com/irvin016/android_external_ant-wireless_antradio-library -b staging/lineage-15.1 external/ant-wireless/antradio-library
 
 
