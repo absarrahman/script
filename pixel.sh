977fc47b7a7d23dc5ce6f5b8f02f33d9e85098bc
Shamith(irvin16) <33453856+irvin016@users.noreply.github.com>
2018-01-08 13:41:40 +0000

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/irvin016/device_xiaomi_kenzo -b o-mr1 device/xiaomi/kenzo
 git clone https://github.com/irvin016/device_xiaomi_msm8956-common -b o-mr1 device/xiaomi/msm8956-common
 git clone https://github.com/irvin016/kernel_xiaomi_msm8956 -b o-mr1 kernel/xiaomi/msm8956
-git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b o-mr1 vendor/xiaomi/kenzo
+git clone https://github.com/irvin016/proprietary_vendor_xiaomi -b o-mr1 vendor/xiaomi
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
 git clone https://github.com/irvin016/android_external_ant-wireless_antradio-library -b staging/lineage-15.1 external/ant-wireless/antradio-library
