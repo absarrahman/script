adbaa309dac9791952be42e129375293c3af773d
Shamith <33453856+shamith16@users.noreply.github.com>
2018-01-15 06:16:23 +0000

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -1,6 +1,7 @@
-git clone https://github.com/irvin016/android_device_xiaomi_kenzo -b o2 device/xiaomi/kenzo
-git clone https://github.com/irvin016/android_kernel_xiaomi_kenzo -b o2 kernel/xiaomi/kenzo
-git clone https://github.com/irvin016/android_vendor_xiaomi_kenzo -b o2 vendor/xiaomi/kenzo
+git clone https://github.com/shamith16/android_device_xiaomi_kenzo -b 8.1 device/xiaomi/kenzo
+git clone https://github.com/shamith16/device_xiaomi_msm8956-common -b 8.1t device/xiaomi/msm8956-common
+git clone https://github.com/shamith16/kernel_xiaomi_msm8956 -b 8.1 kernel/xiaomi/msm8956
+git clone https://github.com/shamith16/proprietary_vendor_xiaomi -b 8.1 vendor/xiaomi
 rm -rf hardware/qcom/audio-caf/msm8952
 rm -rf hardware/qcom/display-caf/msm8952
 rm -rf hardware/qcom/media-caf/msm8952
@@ -9,4 +10,4 @@ git clone https://github.com/Cardinal-AOSP/hardware_qcom_media -b oreo-mr1-caf-8
 git clone https://github.com/Cardinal-AOSP/hardware_qcom_display -b oreo-mr1-caf-8952 hardware/qcom/display-caf/msm8952
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
-git clone https://github.com/irvin016/android_external_ant-wireless_antradio-library -b staging/lineage-15.1 external/ant-wireless/antradio-library
+git clone https://github.com/shamith16/android_external_ant-wireless_antradio-library -b staging/lineage-15.1 external/ant-wireless/antradio-library
