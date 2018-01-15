06261fead64f51b1f50d0711b2b09c9c3e13e16d
Shamith <33453856+shamith16@users.noreply.github.com>
2018-01-15 08:53:40 +0000

    Create potter.sh

---
+++ potter.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/shamith16/MMLC_kernel-msm -b 8.1 kernel/motorola/msm8953
+git clone https://github.com/shamith16/device_motorola_potter -b oreo-mr1 device/motorola/potter
+git clone https://github.com/shamith16/android_vendor_motorola_potter -b lineage-15.1-64 vendor/motorola/potter
