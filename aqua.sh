7e7d3b11c8d44067ac8eaf585aaf290c0a07d123
absarrahman <35573458+absarrahman@users.noreply.github.com>
2018-03-02 19:34:33 +0600

    Create aqua.sh

---
+++ aqua.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/absarrahman/android_device_xiaomi_kenzo -b o2 device/xiaomi/kenzo
+git clone https://github.com/absarrahman/kernel_xiaomi_msm8956 -b 8.1-gcc6 kernel/xiaomi/msm8956
+git clone https://github.com/nitrogen-project/android_vendor_xiaomi_kenzo -b o2 vendor/xiaomi/kenzo
+git clone https://github.com/nitrogen-project/android_hardware_qcom_media-caf-msm8952 -b o2-8952 hardware/qcom/media-caf-msm8952
+git clone https://github.com/nitrogen-project/android_hardware_qcom_audio-caf-msm8952 -b o2-8952 hardware/qcom/audio-caf-msm8952
+git clone https://github.com/nitrogen-project/android_hardware_qcom_display-caf-msm8952 -b o2-8952 hardware/qcom/display-caf-msm8952
+git clone https://bitbucket.org/Sarveshrulz/aarch64-linux-gnu prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu
